e251555588aec89fba97789f49a329a27d2fa54e
Esdras C Paz <101739781+esdras-urgtec@users.noreply.github.com>
2023-02-19 23:17:50 -0300

    58 - Melhoramentos visuais

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\ContatoController;
-use App\Http\Controllers\PrincipalController;
-use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,13 +18,19 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
-Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::prefix('/app')->group(function(){
-    Route::get('/login', function () { return "Login"; })->name('app.applogin');
-    Route::get('/clientes', function () { return "Clientes"; })->name('app.clientes');
-    Route::get('/fornecedores', function () { return "Fornecedores"; })->name('app.fornecedores');
-    Route::get('/produtos', function () { return "Produtos"; })->name('app.produtos');
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
